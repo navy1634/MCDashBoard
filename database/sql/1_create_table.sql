f785cf0ad69d14ad996cd652de4cd78db081485b
kunikeni <ota.kuniya@nifty.co.jp>
2024-08-03 23:28:25 +0900

    Create Sql Commands for docker-container

---
+++ database/sql/1_create_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE PkStatus (
+    number VARCHAR(6) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    ano_name VARCHAR(20) NOT NULL,
+    generation SMALLINT NOT NULL,
+    type1 VARCHAR(20) NOT NULL,
+    type2 VARCHAR(20),
+    ability1 VARCHAR(20) NOT NULL,
+    ability2 VARCHAR(20),
+    abilityHide VARCHAR(20),
+    H SMALLINT NOT NULL,
+    A SMALLINT NOT NULL,
+    B SMALLINT NOT NULL,
+    C SMALLINT NOT NULL,
+    D SMALLINT NOT NULL,
+    S SMALLINT NOT NULL,
+    total SMALLINT NOT NULL,
+    height REAL NOT NULL,
+    weight REAL NOT NULL,
+    egg_group1 VARCHAR(20) NOT NULL,
+    egg_group2 VARCHAR(20)
+);
